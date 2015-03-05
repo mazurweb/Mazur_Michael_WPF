aee5838381fb5714d6bf4ae8b36bfb5c56c144b0
mazurweb <mazurweb@gmail.com>
2015-03-04 22:12:44 -0500

    Revert "Added birthPlace and favoriteHobbies"
    This reverts commit f12eb4f87c5b43f1f5c29581adfaae56b61f4294.

--- Mazur_Michael_Output_Assignment/script.js
+++ Mazur_Michael_Output_Assignment/script.js
@@ -6,7 +6,3 @@
 // 201503 Scalable Data Infrastructures
 // Creating Variables and Output Assignment
 
-var birthPlace = "England"; /*Variable for my Birth Place.  Yes I was born in England awesome right?*/
-
-/*Array for my Favorite Hobbies I know we didn't go over but w/e ;)*/
-var favoriteHobbies = ["Hockey", "Skateboarding", "Developing Website"];

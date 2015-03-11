c3a98b2405cbd423dfa3a9117c0a22183678fb85
mazurweb <mazurweb@gmail.com>
2015-03-10 20:47:17 -0400

    Finished Dog Years Problem

---
+++ Expressions_worksheet/JS/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Mazur on 3/10/15.
+ */
+
+/*
+    Michael Mazur
+    SDF 201503
+    Expressions_worksheet
+ */
+
+//Dog Years Problem Calculating Sparky's Dog Age
+
+//Declare Actual Age
+var actualAge = 3;
+
+//Multiply Actual Age by 7 years to Calculate Dog Years
+var dogAge = actualAge * 7;
+
+//Print out actual and dog years
+console.log("Sparky is " + actualAge + " human years old which is " + dogAge + " in dog years.");
+
+//Slice of Pie Part 1 Problem
+
+

97769995442a2f8ad85e9b8be8259a29f3a2d70c
mazurweb <mazurweb@gmail.com>
2015-03-12 20:39:02 -0400

    Added 3rd prompt
    I added the 3rd prompt for the 3rd side of the triangle

--- Mazur_Michael_Expressions_Assignment/js/expressions.js
+++ Mazur_Michael_Expressions_Assignment/js/expressions.js
@@ -15,10 +15,14 @@
 //Alert the user that calculations for the perimeter for a triangle is about to begin
 alert("Hello!  Welcome to the Triangle Perimeter Calculator App!");
 
-//Prompt User for the First Side of a Triangle
+//Prompt User for the First Side of the Triangle
 
 var side1 = prompt("What is the length of the first side?(In Inches)");
 
 //Prompt User for the Second Side of the Triangle
 
 var side2 = prompt("What is the length of the second side?(In Inches)");
+
+//Prompt user for the Third Side of the Triangle
+
+var side3 = prompt("What is the length of the third side?(Inches)");

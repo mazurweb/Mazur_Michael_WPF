e5ad57668100325bcf3a2779861edf68bb8b548f
mazurweb <mazurweb@gmail.com>
2015-03-12 20:30:55 -0400

    Creation of Expressions HTML and JS
    Created both files and also added my alert and first prompt for the
    user to enter the first side.

---
+++ Mazur_Michael_Expressions_Assignment/js/expressions.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Mazur on 3/12/15.
+ */
+
+//Michael Mazur
+
+//SDI 032015
+
+//Expressions Assignment
+
+// 3-12-15
+
+//Calculating the Perimeter of a Triangle
+
+//Alert the user that calculations for the perimeter for a triangle is about to begin
+alert("Hello!  Welcome to the Triangle Perimeter Calculator App!");
+
+//Prompt User for the First Side of a Triangle
+
+var side1 = prompt("What is length of the first side?(In Inches)");
+
+//Prompt User for the Second Side of the Triangle

667e7be540010160e45de9fe418c21c89f491f1f
mazurweb <mazurweb@gmail.com>
2015-03-19 15:14:19 -0400

    Created 4th area.  And converted it to number

--- Mazur_Michael_Conditionals_Assignment/Mazur_Michael_WPF/js/conditionals.js
+++ Mazur_Michael_Conditionals_Assignment/Mazur_Michael_WPF/js/conditionals.js
@@ -15,16 +15,21 @@ Conditionals Assignment
 var businessName = prompt("What is the name of the establishment you are grading today?");
 
 //Create Prompt for 1st Area which is cleanliness user enters grade from 0-100
-var cleanliness = prompt("Grade the establishments cleanliness from 0-100 (0 being the worst)");
+var cleanliness = prompt("Grade the establishments cleanliness with a grade from 0-100 (0 being the worst)");
 //Convert cleanliness from string to number and resave it to cleanliness
 cleanliness = Number(cleanliness);
 
 //Create Prompt for 2nd Area which is hygiene user enters grade from 0-100
-var hygiene = prompt("Grade the establishments personal hygiene of employees from 0-100 (0 being the worst)");
+var hygiene = prompt("Grade the establishments personal hygiene of employees with a grade from 0-100 (0 being the worst)");
 //Convert hygiene from string to number and resave it to hygiene
 hygiene = Number(hygiene);
 
 //Create Prompt for 3rd Area which is Pests user enters grade from 0-100
-var pests = prompt("Grade the establishments pest control from 0-100 (0 being the worst)");
+var pests = prompt("Grade the establishments pest control with a grade from 0-100 (0 being the worst)");
 //Convert pests from a string to a number and resave it
 pests = Number(pests);
+
+//Create prompt for the 4th area which is maintenance user enters grade from 0-100
+var maintenance = prompt("Grade the establishments up-to-date maintenance of equipment with a grade from 0-100");
+//Convert maintenance from a string to a number and resave it
+maintenance = Number(maintenance);

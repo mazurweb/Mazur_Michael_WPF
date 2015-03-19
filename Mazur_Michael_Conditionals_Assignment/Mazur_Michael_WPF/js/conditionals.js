5eb114dba46ed6d38cc8e34d236f5f805aae5d5f
mazurweb <mazurweb@gmail.com>
2015-03-19 14:56:28 -0400

    Created 2 prompts
    One prompt for the name of the establishment and the 2nd prompt for the
    cleanliness of the establishment.
    Also converted clean to a number instead of a string.

--- Mazur_Michael_Conditionals_Assignment/Mazur_Michael_WPF/js/conditionals.js
+++ Mazur_Michael_Conditionals_Assignment/Mazur_Michael_WPF/js/conditionals.js
@@ -9,3 +9,13 @@ Conditionals Assignment
 
 //Health Inspection Grader
 
+//This Calculator will prompt for 4 areas of healthy practices in restaurants and will consider if they pass or fail
+
+//Prompt User for name of food establishment
+var businessName = prompt("What is the name of the establishment you are grading today?");
+
+//Create Prompt for 1st Area which is Cleanliness user enters grade from 0-100
+var clean = prompt("Grade the establishments cleanliness from 0-100!");
+//Convert cleanliness from string to number and resave it to cleanliness
+clean = Number(clean);
+
